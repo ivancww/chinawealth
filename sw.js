b51e421530ddde19e453b60cb6ef0318fd28f2b7
ivancww <wahchiu0903@gmail.com>
2026-03-19 21:16:07 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = 'asset-v1';
+// 强制更新版本号！从 v2 改成 v4，这会打破 iPad 的旧缓存！
+const CACHE_NAME = 'aia-plan-v4'; 
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting(); // 强制立刻接管，不等待
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          // 清除所有旧版本的缓存 (解决 iPad 看不到新版的问题)
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).then((response) => {
+        if (e.request.url.includes('postimg.cc')) {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, resClone);
+          });
+        }
+        return response;
+      });
+    })
   );
 });
