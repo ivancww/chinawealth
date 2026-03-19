5700cc96bc96cf590717a5f3a45ddea66c48f2cb
ivancww <wahchiu0903@gmail.com>
2026-03-19 19:57:14 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,21 @@
-const CACHE_NAME = 'aia-plan-v2'; // 更新版本號
+const CACHE_NAME = 'asset-v1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap'
 ];
 
-// 安裝時快取基本檔案
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 攔截請求：如果是圖片，則存入快取，下次離線也能看
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request).then((response) => {
-        // 如果請求的是 postimg 的圖片，順便存進快取
-        if (e.request.url.includes('postimg.cc')) {
-          const resClone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(e.request, resClone);
-          });
-        }
-        return response;
-      });
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
